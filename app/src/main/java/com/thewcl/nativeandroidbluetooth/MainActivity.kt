73bc705bb8061f4c372d8fe66c89056e273c6c66
Todd Rylaarsdam <todd@toddr.org>
2021-10-06 21:01:23 -0500

    dark/light theme

--- app/src/main/java/com/thewcl/nativeandroidbluetooth/MainActivity.kt
+++ app/src/main/java/com/thewcl/nativeandroidbluetooth/MainActivity.kt
@@ -1,33 +1,40 @@
 package com.thewcl.nativeandroidbluetooth
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thewcl.nativeandroidbluetooth.ui.theme.NativeAndroidBluetoothTheme
 import java.lang.reflect.Array.set
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            MessageCard(Message("Android", "Jetpack Compose"))
+            NativeAndroidBluetoothTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
+
+
 
 @Composable
 fun MessageCard(msg: Message){
@@ -38,17 +45,35 @@ fun MessageCard(msg: Message){
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Android", "Take a look at Jetpack"))
+    NativeAndroidBluetoothTheme {
+        MessageCard(msg = Message("Android", "Jetpack Compose"))
+    }
 }
